30450b7c2a9b8b1b33dccab9c68bfe833830af92
Dilip prasad <dilipprasad87@gmail.com>
2023-07-06 21:46:01 +0530

    Minor changes

--- captureNewDomains.sh
+++ captureNewDomains.sh
@@ -18,12 +18,18 @@ sort -o /home/pi/addtlPiholeAdlist/StreamingServiceDomains/services/primevideo -
 
 cd /home/pi/addtlPiholeAdlist/StreamingServiceDomains
 
+#Contact and get a single list
 cat ./* > ../combinedlist.txt
 
+
+
+#Navigate to parent directory
 cd ..
 
+#Sort the file and get only unique entries
+sort -o combinedlist.txt -u combinedlist.txt
+
+#Commit and push git changes
 git add .
-
 git commit -m "Update Domains"
-
 git push
