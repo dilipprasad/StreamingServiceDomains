408066eaa8a12b8e5bfe3e4b191f8e261bb2bba2
Automated Script <dilipprasad87@gmail.com>
2023-12-19 10:43:45 +0530

    Modify to use sudo for stop and start adlist

--- block-distractions.sh
+++ block-distractions.sh
@@ -4,5 +4,11 @@
 #
 echo 'blocking distractions...'
 export PATH="$PATH:/usr/sbin:/usr/local/bin/"
-sqlite3 /etc/pihole/gravity.db "update adlist set enabled = true where id = 85;"
+sudo sqlite3 /etc/pihole/gravity.db "update adlist set enabled = true where id = 85;"
 pihole restartdns
+
+
+echo "Sync with gravity sync"
+sudo gravity-sync update
+
+echo "end"
