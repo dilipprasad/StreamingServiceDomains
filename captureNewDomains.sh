a2292a68a7e1455cfca4a7027b767952141b6200
Dilip prasad <dilipprasad87@gmail.com>
2024-11-11 06:29:07 +0530

    Update captureNewDomains.sh

--- captureNewDomains.sh
+++ captureNewDomains.sh
@@ -1,5 +1,5 @@
 \#!/bin/bash
-
+USER=${USER:-$(whoami)}
 executionScriptFolder=/home/$USER/EXECUTIONSCRIPTS/
 streamingServiceDomains="$executionScriptFolder/StreamingServiceDomains"
 piholeBackup=/home/$USER/backup/piholebackup
